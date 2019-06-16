f2d1b52de6be40f7a847ea348e00eae8787a2043
spikeburton <ralphburton3@gmail.com>
2019-06-16 11:26:49 -0400

    finish implementing mySet

--- src/set.js
+++ src/set.js
@@ -2,12 +2,106 @@
  * @file set.js
  * @module mySet
  */
-const Stack = require('./stack');
 
-const mySet = (function() {
-  const collection = Stack();
+const mySet = function() {
+  const collection = [];
 
   return {
-    has: function(el)
-  }
-})()
+    /**
+     * @function has
+     * @param {*} el
+     */
+    has: function(el) {
+      return collection.indexOf(el) !== -1;
+    },
+    /**
+     * @function values
+     */
+    values: function() {
+      return collection;
+    },
+    /**
+     * @function add
+     * @param {*} el
+     * @description Adds an element to the set, if the set does not already contain the element.
+     */
+    add: function(el) {
+      if (!this.has(el)) {
+        collection.push(el);
+        return true;
+      }
+      return false;
+    },
+    /**
+     * @function remove
+     * @param {*} el
+     */
+    remove: function(el) {
+      if (this.has(el)) {
+        const i = collection.indexOf(el);
+        collection.splice(i, 1);
+        return true;
+      }
+      return false;
+    },
+    /**
+     * @function size
+     */
+    size: function() {
+      return collection.length;
+    },
+    /**
+     * @function union
+     * @param {mySet} otherSet
+     * @description Returns a new set which is a union between the two sets.
+     */
+    union: function(otherSet) {
+      const result = mySet();
+
+      const thisValues = this.values();
+      const otherValues = otherSet.values();
+
+      thisValues.forEach(e => result.add(e));
+      otherValues.forEach(e => result.add(e));
+
+      return result;
+    },
+    /**
+     * @function intersection
+     * @param {mySet} otherSet
+     * @description Returns a new set which is an intersection between the two sets.
+     */
+    intersection: function(otherSet) {
+      const result = mySet();
+
+      this.values().forEach(e => {
+        if (otherSet.has(e)) result.add(e);
+      });
+
+      return result;
+    },
+    /**
+     * @function difference
+     * @param {mySet} otherSet
+     */
+    difference: function(otherSet) {
+      const result = mySet();
+
+      this.values().forEach(e => {
+        if (!otherSet.has(e)) result.add(e);
+      });
+
+      return result;
+    },
+    /**
+     * @function subset
+     * @param {mySet} otherSet
+     * @description Test if a set is a subset of another set
+     */
+    subset: function(otherSet) {
+      return this.values().every(e => otherSet.has(e));
+    }
+  };
+};
+
+module.exports = mySet;
