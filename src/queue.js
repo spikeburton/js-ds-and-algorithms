19f7e1e05ade964465b878974dc0698a39a683b9
spikeburton <ralphburton3@gmail.com>
2019-06-16 22:28:31 -0400

    add priority queue tests

--- src/queue.js
+++ src/queue.js
@@ -54,4 +54,66 @@ const Queue = function() {
   };
 };
 
-module.exports = Queue;
+const PriorityQue = function() {
+  const collection = [];
+
+  return {
+    /**
+     * @function print
+     * @description Logs the queue to the console
+     */
+    print: function() {
+      console.log(collection);
+    },
+    /**
+     * @function enqueue
+     * @param {*} el
+     * @description Add an element to the end of the queue
+     */
+    enque: function(el) {
+      if (this.isEmpty()) {
+        collection.push(el)
+      } else {
+        let added = false;
+        for (let i = 0; i < collection.length; i++) {
+          if (el[1] < collection[i][1]) { // Checking priorities
+            collection.splice(i, 0, el)
+            added = true
+            break
+          }
+        }
+        if (!added) collection.push(el)
+      }
+    },
+    /**
+     * @function dequeue
+     * @description Removes the first element from the queue and returns it
+     */
+    dequeue: function() {
+      return collection.shift()[0];
+    },
+    /**
+     * @function front
+     * @description Return the first element in the queue
+     */
+    front: function() {
+      return collection[0];
+    },
+    /**
+     * @function size
+     * @description Return the length of the queue
+     */
+    size: function() {
+      return collection.length;
+    },
+    /**
+     * @function isEmpty
+     * @description Returns true if there are no elements in the queue, false otherwise
+     */
+    isEmpty: function() {
+      return collection.length === 0;
+    }
+  }
+}
+
+module.exports = { Queue, PriorityQue };
--- tests/queue.js
+++ tests/queue.js
@@ -1,18 +1,31 @@
-const Queue = require('../src/queue')
+const { Queue, PriorityQue } = require('../src/queue');
 
 const q1 = Queue();
-console.log(`Queue empty? ${q1.isEmpty()}`)
-console.log(`Size of queue: ${q1.size()}`)
-q1.enqueue(1)
-q1.enqueue(2)
-q1.enqueue(3)
-q1.enqueue(4)
-q1.print()
+console.log(`Queue empty? ${q1.isEmpty()}`);
+console.log(`Size of queue: ${q1.size()}`);
+q1.enqueue(1);
+q1.enqueue(2);
+q1.enqueue(3);
+q1.enqueue(4);
+q1.print();
 
-console.log(`Front of queue: ${q1.front()}`)
-console.log(`Size of queue: ${q1.size()}`)
-console.log(`Dequeue q1: ${q1.dequeue()}`)
-q1.print()
-console.log(`Front of queue: ${q1.front()}`)
-console.log(`Size of queue: ${q1.size()}`)
-console.log(`Queue empty? ${q1.isEmpty()}`)
+console.log(`Front of queue: ${q1.front()}`);
+console.log(`Size of queue: ${q1.size()}`);
+console.log(`Dequeue q1: ${q1.dequeue()}`);
+q1.print();
+console.log(`Front of queue: ${q1.front()}`);
+console.log(`Size of queue: ${q1.size()}`);
+console.log(`Queue empty? ${q1.isEmpty()}`);
+
+console.log('Priority Queue');
+const pq = PriorityQue();
+const el1 = ['Adam Sandler', 2]
+console.log(`El1: ${el1}`)
+pq.enque(el1);
+const el2 = ['Alan Turing', 3]
+console.log(`El1: ${el2}`)
+pq.enque(el2);
+const el3 = ['Barack Obama', 1]
+console.log(`El1: ${el3}`)
+pq.enque(el3);
+pq.print();
