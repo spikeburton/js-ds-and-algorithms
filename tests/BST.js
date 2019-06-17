8de3d33710f227e48048b46e5a9e5725923c3b0f
spikeburton <ralphburton3@gmail.com>
2019-06-17 00:49:42 -0400

    implement tests for binary search trees

---
+++ tests/BST.js
@@ -0,0 +1,22 @@
+const { BST } = require('../src/BST');
+
+const bst = new BST();
+
+bst.add(4);
+bst.add(2);
+bst.add(6);
+bst.add(1);
+bst.add(3);
+bst.add(5);
+bst.add(7);
+console.log(`Tree: `, bst);
+console.log('Remove: 4');
+bst.remove(4);
+console.log(`4 is present: `, bst.isPresent(4));
+console.log(`Tree now: `, bst);
+console.log(`Min: `, bst.findMin());
+console.log(`Max: `, bst.findMax());
+console.log('Remove: 7')
+bst.remove(7);
+console.log(`Max: `, bst.findMax());
+console.log(`Tree now: `, bst);
