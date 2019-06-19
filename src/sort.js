373ce9335230c88275709396dd9c42725858c018
spikeburton <ralphburton3@gmail.com>
2019-06-18 23:03:00 -0400

    add documentation for insertion sort

--- src/sort.js
+++ src/sort.js
@@ -32,12 +32,18 @@ function bubbleSort(arr) {
  */
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
+    // KEY is the element to insert
     let key = arr[i];
     let j = i - 1;
+    // LIST is already sorted to the left of the key, so find the first element
+    // smaller and insert there
     while (j >= 0 && arr[j] > key) {
+      // COPY the element at index j into arr[j+1] - note that this starts at arr[i]
+      // SHUFFLE all the elements to the right until inserted into the correct spot
       arr[j + 1] = arr[j];
       j = j - 1;
     }
+    // FINALLY copy the key into the spot where it belongs
     arr[j + 1] = key;
   }
   return arr;
