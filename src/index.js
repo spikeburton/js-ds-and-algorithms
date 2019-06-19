e3ff2e5b7c6744f60167e86f3e07331ec01b9239
spikeburton <ralphburton3@gmail.com>
2019-06-18 21:34:27 -0400

    add bubble sort

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { Trie } = require('./Trie')
 const { MinHeap } = require('./Heap')
 const { bfs } = require('./Graph')
+const { bubbleSort } = require('./sort')
 
-module.exports = { Trie, MinHeap, bfs }
+module.exports = { Trie, MinHeap, bfs, bubbleSort }
