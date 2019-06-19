1d8b3d3eb8028beedda02a8e8bc3f85fe9474bf9
spikeburton <ralphburton3@gmail.com>
2019-06-18 22:00:45 -0400

    implement insertion sort

--- src/sort.js
+++ src/sort.js
@@ -12,18 +12,35 @@ function bubbleSort(arr) {
   let swap = false;
 
   do {
-    swap = false
+    swap = false;
     for (let i = 0; i < arr.length; i++) {
       if (arr[i] > arr[i + 1]) {
-        swap = true
-        const temp = arr[i + 1]
-        arr[i + 1] = arr[i]
-        arr[i] = temp
+        swap = true;
+        const temp = arr[i + 1];
+        arr[i + 1] = arr[i];
+        arr[i] = temp;
       }
     }
-  } while (swap)
+  } while (swap);
 
   return arr;
 }
 
-module.exports = { bubbleSort }
+/**
+ * @function insertionSort
+ * @param {Array} arr
+ */
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+module.exports = { bubbleSort, insertionSort };
