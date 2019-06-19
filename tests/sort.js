ce45bb6b2d0e6b75b6d6681cc6582d7169f149e9
spikeburton <ralphburton3@gmail.com>
2019-06-18 23:36:07 -0400

    add test for merge sort

--- tests/sort.js
+++ tests/sort.js
@@ -1,6 +1,12 @@
-const { bubbleSort } = require('../src')
+const { bubbleSort, insertionSort, mergeSort } = require('../src')
 
 const nums = [5, 10, 1, 9, 2, 8, 3, 7, 4, 6]
+let result;
 console.log('Nums: ', nums)
 
-console.log('Bubble sort: ', bubbleSort(nums))
+// result = bubbleSort(nums)
+// console.log('Bubble sort: ', result)
+// result = insertionSort(nums)
+// console.log('Insertion sort: ', result)
+result = mergeSort(nums)
+console.log('Merge sort: ', result)
