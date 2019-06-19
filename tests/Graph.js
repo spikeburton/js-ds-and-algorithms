0f2469233ea498b117e2f2600c1a72a17a9b0e29
spikeburton <ralphburton3@gmail.com>
2019-06-18 21:17:02 -0400

    add tests for breadth-first search through a graph

---
+++ tests/Graph.js
@@ -0,0 +1,11 @@
+const { bfs } = require('../src');
+
+const exBFSGraph = [
+  [0, 1, 1, 1, 0],
+  [0, 0, 1, 0, 0],
+  [1, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0],
+  [0, 1, 0, 0, 0]
+];
+
+console.log(bfs(exBFSGraph, 1))
